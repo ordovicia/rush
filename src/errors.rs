8dfc56ecf356757c03f828baca858a74b52eace1
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-09-24 18:08:15 +0900

    Polish Error

--- src/errors.rs
+++ src/errors.rs
@@ -1,10 +1,28 @@
-use std::result;
+use std::{result, io};
 
-#[derive(Debug, PartialEq)]
-pub enum Error {
-    ParseJob,
-    SpawnChild,
-    WaitChild,
-}
+use nom;
 
 pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    ParseError(ParseError),
+    ExecuteError(io::Error),
+}
+
+impl PartialEq for Error {
+    fn eq(&self, other: &Self) -> bool {
+        use self::Error::ExecuteError;
+
+        match (self, other) {
+            (&ExecuteError(_), &ExecuteError(_)) => true,
+            (s, o) => s == o,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    Error(nom::Err<u32>),
+    Incomplete(nom::Needed),
+}
